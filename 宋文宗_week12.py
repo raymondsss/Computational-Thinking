6b42d2bee97ae44b7076c986008f0b6e40d155d4
raymondsss <71250893+raymondsss@users.noreply.github.com>
2020-11-30 14:51:34 +0800

    Add files via upload

---
+++ 宋文宗_week12.py
@@ -0,0 +1,34 @@
+hello1 = "Hello World"
+hello2 = 'Hello Python'
+hello3 = "Shaquile O'neil"
+
+s1 = "123"
+s2 = "456"
+s3 = s1 + s2
+print(s3)
+s4 = s1 * 2
+print(s4)
+
+s1 = 'An apple a day.'
+s1.capitalize()
+s1.title()
+s1.swapcase()
+s1.upper()
+s1.lower
+numberlist = [1,2,3,4,5]
+charlist = ["1","2","3","4","5"]
+mixlist = [1,"2",3,"4",5]
+
+word = "Python"
+list1 = list(word)
+list2 = ["color","animals",1,2,3]
+
+season = "1995-1996"
+team = "Chicago Bulls"
+coach = "Phil Jackson"
+records = [72, 10]
+starting_lineup = ["Ron Harper", "Michael Jordan", "ScottiePippen", "Dennis Rodman", "Luc Longley"]
+won_championship = True
+best_chicago_bulls = list((season, team, coach, records, starting_lineup, won_championship))
+print(best_chicago_bulls)
+print(type(best_chicago_bulls))
